fe05f896fb48a481633129f62fffb3c08cae6787
Ishmet <ishmetjahan@yahoo.com>
2019-06-22 18:20:28 +0000

    Automatically backed up by Learn

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -1,1 +1,12 @@
 # Write your code here.
+def word_substituter (str)
+  str.split (" ").map do |word|
+    if dictionary.keys.include? (word.downcase)
+      word = dictionary[word.downcase]
+    else
+      word
+    end
+  end
+  str.join(" ")
+  
+end
