f7752d409e777499d1c23ec2586f8735cd07cfcb
Ishmet <ishmetjahan@yahoo.com>
2019-06-22 18:26:20 +0000

    Automatically backed up by Learn

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -1,5 +1,16 @@
 def dictionary
-  
+  {
+    "hello" => 'hi',
+    "to"=> "2",
+    "too"=> "2",
+    "two" => "2",
+    "for"=>"4",
+    "four" => "4", 
+    'be' => "b",
+    'you'=> "u",
+    "at" =>"@",
+    "and" => "&",
+    }
 end
 
 # Write your code here.
